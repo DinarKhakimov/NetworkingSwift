3db13ebda6b2bebc34de61cb60af6bddedf002fc
Johnny Boshechka <56588291+JohnnyBoshechka@users.noreply.github.com>
2022-01-23 10:44:50 +0500

    Initial Commit

---
+++ NetworkingSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingSwift
+//
+//  Created by Johnny Boshechka on 1/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
